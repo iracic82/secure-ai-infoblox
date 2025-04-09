29c44683b33fbc6c229c89fc8cd57fb211bb1b73
Igor Racic <iracic@ib-jpd49t2f97.localdomain>
2025-04-09 14:53:38 +0200

    Add aws-user-data.sh

--- scripts/aws-user-data.sh
+++ scripts/aws-user-data.sh
@@ -9,11 +9,12 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo curl https://igor-prosimo.s3.eu-west-1.amazonaws.com/network_testing.py -o /home/ec2-user/network_testing.py
 sudo docker pull iracic82/prosimo-flask-app-labs:latest
+sudo docker pull iracic82/secure-ai-dns-demo:latest
 sudo docker pull iracic82/prosimo-iperf3:latest
 sudo docker pull iracic82/prosimo-postgresql:latest
 sudo docker pull iracic82/prosimo-flask-sqlclient:latest
 sudo docker pull iracic82/prosimo-security-api:latest
-sudo docker run -d -p 5000:5000 iracic82/prosimo-flask-sqlclient:latest
+sudo docker run -d -p 5000:5000 -e FLASK_SECRET_KEY=supersecurekey9876   -e BEDROCK_REGION=eu-west-2   -e BEDROCK_MODEL_ID=amazon.titan-text-lite-v1   iracic82/secure-ai-dns-demo:latest
 sudo docker run -d --name iperf-server -p 5201:5201/tcp -p 5201:5201/udp -p 5201:5201/sctp iracic82/prosimo-iperf3:latest -s
 
 cat <<"EOT" > /home/ec2-user/run_script.sh
